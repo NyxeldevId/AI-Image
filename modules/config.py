7fd4d1f0bf084a76a71aae14e495d6c083f1e282
Aditya DN <Adityadn@users.noreply.huggingface.co>
2024-03-16 15:07:17 +0000

    Update modules/config.py

--- modules/config.py
+++ modules/config.py
@@ -351,9 +351,13 @@ embeddings_downloads = get_config_item_or_set_default(
 available_aspect_ratios = get_config_item_or_set_default(
     key='available_aspect_ratios',
     default_value=[
-        '1080*1920', '800*1000', '1080*1080', '2560*2048', '6000*4000', '1024*768',
-        '1920*1080', '3440*1440', '5760*1920'
+        '704*1408', '704*1344', '768*1344', '768*1280', '832*1216', '832*1152',
+        '896*1152', '896*1088', '960*1088', '960*1024', '1024*1024', '1024*960',
+        '1088*960', '1088*896', '1152*896', '1152*832', '1216*832', '1280*768',
+        '1344*768', '1344*704', '1408*704', '1472*704', '1536*640', '1600*640',
+        '1664*576', '1728*576'
     ],
+
     validator=lambda x: isinstance(x, list) and all('*' in v for v in x) and len(x) > 1
 )
 default_aspect_ratio = get_config_item_or_set_default(
